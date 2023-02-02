09677bbea075e175aae94eac082dd853789f1489
evangambit <evangambit@gmail.com>
2023-02-01 22:52:43 -0800

    x

--- src/main.cpp
+++ src/main.cpp
@@ -316,7 +316,7 @@ std::pair<Evaluation, Move> qsearch(Position *pos, int32_t depth, Evaluation alp
 }
 
 template<Color TURN>
-std::pair<Evaluation, Move> search(Position* pos, const Depth depth, const bool isPV, Evaluation alpha, const Evaluation beta, RecommendedMoves recommendedMoves) {
+std::pair<Evaluation, Move> search(Position* pos, const Depth depth, Evaluation alpha, const Evaluation beta, RecommendedMoves recommendedMoves) {
   constexpr Color opposingColor = opposite_color<TURN>();
   constexpr ColoredPiece moverKing = coloredPiece<TURN, Piece::KING>();
 
@@ -408,7 +408,6 @@ std::pair<Evaluation, Move> search(Position* pos, const Depth depth, const bool 
   RecommendedMoves recommendationsForChildren;
 
   size_t numValidMoves = 0;
-  bool isChildPV = isPV;
   for (ExtMove *extMove = moves; extMove < end; ++extMove) {
 
     #ifndef NDEBUG
@@ -430,8 +429,7 @@ std::pair<Evaluation, Move> search(Position* pos, const Depth depth, const bool 
 
     ++numValidMoves;
 
-    std::pair<Evaluation, Move> a = search<opposingColor>(pos, depth - 1, isChildPV, -beta, -alpha, recommendationsForChildren);
-    isChildPV = false;
+    std::pair<Evaluation, Move> a = search<opposingColor>(pos, depth - 1, -beta, -alpha, recommendationsForChildren);
     a.first *= -1;
     if (a.first > kMaxEval - 100) {
       a.first -= 1;
@@ -525,9 +523,9 @@ std::pair<Evaluation, Move> search(Position* pos, const Depth depth, const bool 
 // Gives scores from white's perspective
 std::pair<Evaluation, Move> search(Position* pos, Depth depth) {
   if (pos->turn_ == Color::WHITE) {
-    return search<Color::WHITE>(pos, depth, true, kMinEval, kMaxEval, RecommendedMoves());
+    return search<Color::WHITE>(pos, depth, kMinEval, kMaxEval, RecommendedMoves());
   } else {
-    std::pair<Evaluation, Move> r = search<Color::BLACK>(pos, depth, true, kMinEval, kMaxEval, RecommendedMoves());
+    std::pair<Evaluation, Move> r = search<Color::BLACK>(pos, depth, kMinEval, kMaxEval, RecommendedMoves());
     r.first *= -1;
     return r;
   }
