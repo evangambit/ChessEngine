5c2c5c4e64b3faa08d87b375ee3b16faeb3a25eb
Morgan Redding <mredding@google.com>
2023-03-29 20:04:09 +0800

    Add small tempo bonus when futility pruning

--- src/main.cpp
+++ src/main.cpp
@@ -436,8 +436,11 @@ SearchResult<TURN> search(
       return SearchResult<TURN>(cr.eval, cr.bestMove);
     }
   } else if (depth == 1) {
+    // We assume that you can make a move that improves your position, so comparing against alpha
+    // gets a small bonus.
+    const Evaluation tempoMargin = 20;
     SearchResult<TURN> r = qsearch<TURN>(pos, 0, alpha, beta);
-    if (r.score >= beta + futilityThreshold || r.score <= alpha - futilityThreshold) {
+    if (r.score >= beta + futilityThreshold || r.score + tempo <= alpha - futilityThreshold) {
       return r;
     }
   }
