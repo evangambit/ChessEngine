744add5fd0dc887b4e0c2853d9b626e782007f9f
Morgan Redding <evangambit@gmail.com>
2023-10-17 01:20:14 -0700

    Make build.sh useful

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-
-g++ src/main.cpp src/game/*.cpp -std=c++20 -O3 -DNDEBUG -o main
-
-g++ src/uci.cpp src/game/*.cpp -std=c++20 -O3 -DNDEBUG -o uci
-
-# g++ src/selfplay.cpp src/game/*.cpp -std=c++20 -O3 -DNDEBUG -o selfplay
+g++ src/game/*.cpp src/protos/weights.pb.cc \
+-std=c++20 \
+-O3 \
+-DNDEBUG \
+-lprotobuf \
+-L /opt/homebrew/Cellar/protobuf/24.4/lib \
+-L /opt/homebrew/Cellar/abseil/20230802.1/lib \
+-I /opt/homebrew/Cellar/protobuf/24.4/include \
+-I /opt/homebrew/Cellar/abseil/20230802.1/include \
+-I src $@
