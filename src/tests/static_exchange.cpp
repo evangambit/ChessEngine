92dd121b87a058c92a4a8895d0de99beab21bd3f
Morgan Redding <evangambit@gmail.com>
2024-01-01 18:53:23 +0800

    Migrate static_exchange to gtest

--- src/tests/static_exchange.cpp
+++ src/tests/static_exchange.cpp
@@ -1,5 +1,6 @@
-// g++ src/tests/static_exchange.cpp src/game/*.cpp -std=c++20 -o static_exchange_tests
+// g++ src/tests/static_exchange.cpp src/game/*.cpp -std=c++20 -I/opt/homebrew/Cellar/googletest/1.14.0/include -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest -o static_exchange_tests
 
+#include "gtest/gtest.h"
 
 #include <cassert>
 #include <cstdint>
@@ -19,48 +20,33 @@ constexpr int kKnightValue = 3;
 
 // typedef StaticExchangeAnalysis::static_exchange static_exchange;
 
-template<class T>
-void assert_equal(const T& a, const T& b, const std::string failureMessage = "") {
-  if (a != b) {
-    std::cout << "Assertion Failed: " << a << " != " << b << std::endl;
-    if (failureMessage.size() > 0) {
-      std::cout << failureMessage << std::endl;
-    }
-  }
-}
-
-void test1() {
+TEST(StaticExchange, Test1) {
   Position pos("8/1k6/8/8/5q2/8/1K3R2/8 w - - 0 1");
-  assert_equal(
+  ASSERT_EQ(
     StaticExchangeAnalysis::static_exchange<Color::WHITE>(&pos),
-    kQueenValue,
-    "test1 failed"
+    kQueenValue
   );
 }
 
-void test2() {
+TEST(StaticExchange, Test2) {
   Position pos("8/1k6/8/4p3/5q2/8/1K3R2/8 w - - 0 1");
-  assert_equal(
+  ASSERT_EQ(
     StaticExchangeAnalysis::static_exchange<Color::WHITE>(&pos),
-    kQueenValue - kRookValue,
-    "test2 failed"
+    kQueenValue - kRookValue
   );
 }
 
-void test3() {
+TEST(StaticExchange, Test3) {
   // Nxf4  # rook takes queen
   // Rxf4  # knight takes rook
   Position pos("8/1k6/8/8/5q2/1K1n4/5R2/8 w - - 0 1");
-  assert_equal(
+  ASSERT_EQ(
     StaticExchangeAnalysis::static_exchange<Color::WHITE>(&pos),
-    kQueenValue - kRookValue,
-    "test3 failed"
+    kQueenValue - kRookValue
   );
 }
 
 int main(int argc, char *argv[]) {
-  test1();
-  test2();
-  test3();
-  return 0;
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
