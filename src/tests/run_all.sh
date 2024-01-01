7d04249256c65040f26cc9e0c985fa69388c532a
Morgan Redding <evangambit@gmail.com>
2024-01-02 03:11:46 +0800

    Clean up run_all.sh

--- src/tests/run_all.sh
+++ src/tests/run_all.sh
@@ -1,13 +1,11 @@
 
-alias foo='g++ -std=c++20 -I/opt/homebrew/Cellar/googletest/1.14.0/include -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest -o test src/game/*.cpp'
-
-for f in ./*.c; do echo "Processing $f file..."; done
-
-
 for test in ./src/tests/*.cpp
 do
   echo $test
-  foo $test
+  g++ -std=c++20 \
+  -I/opt/homebrew/Cellar/googletest/1.14.0/include \
+  -L/opt/homebrew/Cellar/googletest/1.14.0/lib -lgtest \
+  -o test src/game/*.cpp $test
   ./test
 done
 
