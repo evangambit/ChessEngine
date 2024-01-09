6532278f422976e1c234b4d80e73e5f5bfab3c31
Morgan Redding <evangambit@gmail.com>
2024-01-09 07:26:45 -0800

    Add helper for printing sets to utils

--- src/game/utils.h
+++ src/game/utils.h
@@ -7,6 +7,7 @@
 #include <deque>
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 namespace ChessEngine {
 
@@ -151,11 +152,28 @@ std::vector<std::string> split(const std::string& text, char delimiter);
 template<class T>
 std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vec) {
   if (vec.size() == 0) {
-    return stream << "{}";
+    return stream << "[]";
   }
-  stream << "{" << vec[0];
+  stream << "[" << vec[0];
   for (size_t i = 1; i < vec.size(); ++i) {
     stream << ", " << vec[i];
+  }
+  return stream << "]";
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& stream, const std::unordered_set<T>& set) {
+  if (set.size() == 0) {
+    return stream << "{}";
+  }
+  stream << "{";
+  bool isFirst = true;
+  for (const auto& val : set) {
+    if (!isFirst) {
+      std::cout << ", ";
+    }
+    stream << val;
+    isFirst = true;
   }
   return stream << "}";
 }
