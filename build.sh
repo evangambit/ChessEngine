28d9efcbb11dfe3f7ab18a1b648bfc2ad245a4f6
Morgan Redding <evangambit@gmail.com>
2024-06-15 16:28:00 -0700

    save some build flags for later

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ g++ src/game/*.cpp \
 -DNDEBUG \
 -I src $@
 
+# -I /opt/homebrew/Cellar/eigen/3.4.0_1/include \
 # protoc protos/weights.proto --cpp_out src --python_out ./
+
+# Production:
+# -O3 -DNDEBUG
+# 
+# Debug:
+# -rdynamic -g1
