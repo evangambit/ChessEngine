87cc2570707052eb908b4d99f47090b8cf3a5d17
evangambit <evangambit@gmail.com>
2024-11-07 17:02:35 -0800

    Make nnue.h support bfloat16 (sticking with float bc its faster...)

--- src/game/nnue.h
+++ src/game/nnue.h
@@ -11,6 +11,10 @@ using Eigen::Matrix;
 
 namespace ChessEngine {
 
+typedef float MatType;
+
+static MatType kZero = MatType(0.0);
+
 enum NnueFeatures {
   NF_WHITE_PAWN_A8 = 0,
   NF_WHITE_PAWN_B8 = 1,
@@ -72,42 +76,50 @@ struct DummyNetwork : public NnueNetworkInterface {
 
 struct NnueNetwork : public NnueNetworkInterface {
   static constexpr int kInputDim = 12 * 8 * 8 + 8;
-  static constexpr int kWidth1 = 512;
-  static constexpr int kWidth2 = 64;
+  static constexpr int kWidth1 = 1024;
+  static constexpr int kWidth2 = 128;
   static constexpr int kWidth3 = 1;
 
-  Matrix<float, 1, Eigen::Dynamic> x0;  // 1 x 64*12  (768)
-  Matrix<float, Eigen::Dynamic, kWidth1> w0;  // kInputDim x kWidth1
-  Matrix<float, 1, kWidth1> b0;
+  Matrix<MatType, 1, Eigen::Dynamic> x0;  // 1 x 64*12  (768)
+  Matrix<MatType, Eigen::Dynamic, kWidth1> w0;  // kInputDim x kWidth1
+  Matrix<MatType, 1, kWidth1> b0;
 
-  Matrix<float, 1, kWidth1> x1;
-  Matrix<float, 1, kWidth1> x1_relu;
-  Matrix<float, Eigen::Dynamic, kWidth2> w1;  // kWidth1 x kWidth2
-  Matrix<float, 1, kWidth2> b1;
+  Matrix<MatType, 1, kWidth1> x1;
+  Matrix<MatType, 1, kWidth1> x1_relu;
+  Matrix<MatType, Eigen::Dynamic, kWidth2> w1;  // kWidth1 x kWidth2
+  Matrix<MatType, 1, kWidth2> b1;
 
-  Matrix<float, 1, kWidth2> x2;
-  Matrix<float, Eigen::Dynamic, kWidth3> w2;  // kWidth2 x kWidth3
-  Matrix<float, 1, kWidth3> b2;
+  Matrix<MatType, 1, kWidth2> x2;
+  Matrix<MatType, Eigen::Dynamic, kWidth3> w2;  // kWidth2 x kWidth3
+  Matrix<MatType, 1, kWidth3> b2;
 
-  Matrix<float, 1, kWidth3> x3;
+  Matrix<MatType, 1, kWidth3> x3;
+
+  // info depth 1 multipv 1 score cp 1419 nodes 0 nps 0 time 4 pv d2d4
+  // info depth 2 multipv 1 score cp 1289 nodes 253 nps 14882 time 17 pv e2e4 e7e5
+  // info depth 3 multipv 1 score cp 920 nodes 610 nps 14878 time 41 pv e2e4 e7e5 d2d4 e5d4
+  // info depth 4 multipv 1 score cp 1587 nodes 2429 nps 19277 time 126 pv e2e4 c7c5 g1f3 d7d6
+  // info depth 5 multipv 1 score cp 1351 nodes 6552 nps 20284 time 323 pv e2e4 e7e5 g1f3 b8c6 f1b5
+  // info depth 6 multipv 1 score cp 1418 nodes 35967 nps 21244 time 1693 pv e2e4 e7e6 g1f3 d7d5 b1c3 d5e4 c3e4
+  // bestmove e2e4 ponder e7e6
 
   NnueNetwork() {
-    w0 = Matrix<float, Eigen::Dynamic, kWidth1>::Zero(NnueFeatures::NF_NUM_FEATURES, kWidth1);
-    w1 = Matrix<float, Eigen::Dynamic, kWidth2>::Zero(kWidth1, kWidth2);
-    w2 = Matrix<float, Eigen::Dynamic, kWidth3>::Zero(kWidth2, kWidth3);
+    w0 = Matrix<MatType, Eigen::Dynamic, kWidth1>::Zero(NnueFeatures::NF_NUM_FEATURES, kWidth1);
+    w1 = Matrix<MatType, Eigen::Dynamic, kWidth2>::Zero(kWidth1, kWidth2);
+    w2 = Matrix<MatType, Eigen::Dynamic, kWidth3>::Zero(kWidth2, kWidth3);
 
-    x0 = Matrix<float, 1, Eigen::Dynamic>::Zero(1, NnueFeatures::NF_NUM_FEATURES);
-    x1 = Matrix<float, 1, kWidth1>::Zero(1, kWidth1);
-    x1_relu = Matrix<float, 1, kWidth1>::Zero(1, kWidth1);
-    x2 = Matrix<float, 1, kWidth2>::Zero(1, kWidth2);
-    x3 = Matrix<float, 1, kWidth3>::Zero(1, kWidth3);
+    x0 = Matrix<MatType, 1, Eigen::Dynamic>::Zero(1, NnueFeatures::NF_NUM_FEATURES);
+    x1 = Matrix<MatType, 1, kWidth1>::Zero(1, kWidth1);
+    x1_relu = Matrix<MatType, 1, kWidth1>::Zero(1, kWidth1);
+    x2 = Matrix<MatType, 1, kWidth2>::Zero(1, kWidth2);
+    x3 = Matrix<MatType, 1, kWidth3>::Zero(1, kWidth3);
 
-    b0 = Matrix<float, 1, kWidth1>::Zero(1, kWidth1);
-    b1 = Matrix<float, 1, kWidth2>::Zero(1, kWidth2);
-    b2 = Matrix<float, 1, kWidth3>::Zero(1, kWidth3);
+    b0 = Matrix<MatType, 1, kWidth1>::Zero(1, kWidth1);
+    b1 = Matrix<MatType, 1, kWidth2>::Zero(1, kWidth2);
+    b2 = Matrix<MatType, 1, kWidth3>::Zero(1, kWidth3);
 
     std::ifstream myfile;
-    myfile.open("nnue-776-512-64.bin", std::ios::in | std::ios::binary);
+    myfile.open("nnue-776-1024-128.bin", std::ios::in | std::ios::binary);
     if (!myfile.is_open()) {
       std::cout << "Error opening file \"nnue-776-512-64.bin\"" << std::endl;
       exit(0);
@@ -128,36 +140,41 @@ struct NnueNetwork : public NnueNetworkInterface {
     return this->fastforward();
   }
   float fastforward() {
-    x1_relu.noalias() = x1.unaryExpr([](float x) -> float {
-      return x > 0 ? x : 0;
+    x1_relu.noalias() = x1.unaryExpr([](MatType x) -> MatType {
+      return x > kZero ? x : kZero;
     });
 
     x2.noalias() = x1_relu * w1;
     x2.noalias() += b1;
-    x2.noalias() = x2.unaryExpr([](float x) -> float {
-      return x > 0 ? x : 0;
+    x2.noalias() = x2.unaryExpr([](MatType x) -> MatType {
+      return x > kZero ? x : kZero;
     });
     x3.noalias() = x2 * w2;
     x3.noalias() += b2;
     return x3(0, 0);
   }
 
-  // void load(std::string filename) {
-  //   FILE *f = fopen(filename.c_str(), "rb");
-  //   fread(w0.data(), sizeof(float), w0.size(), f);
-  //   fread(w1.data(), sizeof(float), w1.size(), f);
-  //   fread(w2.data(), sizeof(float), w2.size(), f);
-  //   fread(b0.data(), sizeof(float), b0.size(), f);
-  //   fread(b1.data(), sizeof(float), b1.size(), f);
-  //   fclose(f);
-  // }
+  template<int H, int W>
+  void _load(std::istream& file, Eigen::Matrix<MatType, H, W>& out) {
+    const size_t N = out.size();
+    for (size_t i = 0; i < N; ++i) {
+      float value;
+      file.read(reinterpret_cast<char*>(&value), sizeof(float));
+      reinterpret_cast<MatType*>(out.data())[i] = MatType(value);
+    }
+  }
 
   void load(std::istream& myfile) {
-    myfile.read(reinterpret_cast<char*>(w0.data()), w0.size() * sizeof(float));
-    myfile.read(reinterpret_cast<char*>(w1.data()), w1.size() * sizeof(float));
-    myfile.read(reinterpret_cast<char*>(w2.data()), w2.size() * sizeof(float));
-    myfile.read(reinterpret_cast<char*>(b0.data()), b0.size() * sizeof(float));
-    myfile.read(reinterpret_cast<char*>(b1.data()), b1.size() * sizeof(float));
+    // myfile.read(reinterpret_cast<char*>(w0.data()), w0.size() * sizeof(float));
+    // myfile.read(reinterpret_cast<char*>(w1.data()), w1.size() * sizeof(float));
+    // myfile.read(reinterpret_cast<char*>(w2.data()), w2.size() * sizeof(float));
+    // myfile.read(reinterpret_cast<char*>(b0.data()), b0.size() * sizeof(float));
+    // myfile.read(reinterpret_cast<char*>(b1.data()), b1.size() * sizeof(float));
+    _load(myfile, w0);
+    _load(myfile, w1);
+    _load(myfile, w2);
+    _load(myfile, b0);
+    _load(myfile, b1);
   }
 
   void set_piece(ColoredPiece piece, Square square, float newValue) {
@@ -171,7 +188,7 @@ struct NnueNetwork : public NnueNetworkInterface {
   }
 
   void set_index(size_t index, float newValue) {
-    float delta = newValue - x0(0, index);
+    MatType delta = MatType(newValue) - x0(0, index);
     if (delta == 0.0) {
       return;
     }
@@ -181,7 +198,7 @@ struct NnueNetwork : public NnueNetworkInterface {
     } else if (delta == -1.0) {
       x1.row(0).noalias() -= w0.row(index);
     } else {
-      x1.row(0).noalias() += w0.row(index) * delta;
+      x1.row(0).noalias() += w0.row(index) * MatType(delta);
     }
   }
 };
